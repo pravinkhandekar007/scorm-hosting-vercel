25472bc49652dbf92c03c8ec37f30d0fe9c2689b
pravinkhandekar007 <pravinlk@outlook.com>
2025-11-02 18:43:59 +0530

    Update enrollments.js

--- api/enrollments.js
+++ api/enrollments.js
@@ -3,6 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+const frontendUrl = process.env.FRONTEND_BASE_URL;
 
 async function fetchUserByEmail(email) {
   const { data, error } = await supabase.auth.admin.listUsers({ email });
